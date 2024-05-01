d79a5b40456c65ff7db61acc17ebd49b32275c67
Melik <meliksahin05@gmail.com>
2024-05-01 21:41:52 +0300

    v1

---
+++ src/main/java/Class01.java
@@ -0,0 +1,14 @@
+public class Class01 {
+
+    public static void main(String[] args) {
+
+       //CODES FOR VERSION 1
+        int x = 1;
+        int y = 2;
+        System.out.println(x+y);
+        System.out.println("Done with codes for version1");
+
+
+
+    }
+}
